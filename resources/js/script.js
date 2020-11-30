d43704c363a3378923ab0ecd7bea6520dc1950f4
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2020-11-30 15:45:53 +0000

    Corrected Responsiveness & Overflow Problems, changed membership names

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,3 +2,12 @@ function sayHello() {
     alert("Thank you for joining! A member of the team will be in touch soon to discuss your induction");
     return true;
 }
+
+function SuccessfulSignUp() {
+    alert("Thank you for signing up! A member of our team will get in touch with you soon!");
+    return true;
+}
+
+$('details').click(function(event) {
+    $('details').not(this).removeAttr("open");
+});
